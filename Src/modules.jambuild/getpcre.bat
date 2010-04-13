3409bbc57ae436f1bd6530b24d28d5ab739485ce
Joshua Jensen <jjensen@workspacewhiz.com>
2010-04-12 22:56:43 -0600

    * Fix the Windows Jam build after the Linux fix ups.

--- Src/modules.jambuild/getpcre.bat
+++ Src/modules.jambuild/getpcre.bat
@@ -1,3 +1,5 @@
+pushd
+cd ..\Modules\lrexlib
 if exist pcre-7.8.zip del pcre-7.8.zip
 if exist pcre-7.8 rmdir /s /q pcre-7.8
 if exist pcre-8.00.zip del pcre-8.00.zip
@@ -13,3 +15,4 @@ if not exist pcre.h copy pcre.h.generic pcre.h
 if not exist config.h copy config.h.generic config.h
 if not exist pcre_chartables.c copy pcre_chartables.c.dist pcre_chartables.c
 cd ..
+popd
