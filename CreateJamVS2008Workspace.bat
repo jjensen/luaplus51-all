8eccb1c2f84963e81a3542163002ad5d1079ab24
Joshua Jensen <jjensen@workspacewhiz.com>
2010-09-09 23:03:29 -0600

    * Fixed up CreateJamVS2008Workspace.bat to use the VS2008 compiler.

--- CreateJamVS2008Workspace.bat
+++ CreateJamVS2008Workspace.bat
@@ -1,1 +1,1 @@
-@jam --workspace -gen=vs2008 -config=CreateJamVS2008Workspace.config Jamfile.jam build
+@jam --workspace -gen=vs2008 -config=CreateJamVS2008Workspace.config --compiler=vs2008 Jamfile.jam build2008
