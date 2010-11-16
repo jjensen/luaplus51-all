adfc8cb82c0248f21a3621c1dd6444773d170272
Joshua Jensen <jjensen@workspacewhiz.com>
2010-11-15 20:52:01 -0700

    * Upgrade to the latest lqt.

--- Src/Modules/lqt/generator/qtemplates.lua
+++ Src/Modules/lqt/generator/qtemplates.lua
@@ -5,10 +5,18 @@ return {
 	-- module = { ["ex2<K,V>"] = { "example<char*, int>" } }
 	
 	qtcore = {
-		["QList<T>"] = { "QList<QString>", "QList<QFileInfo>" },
+		["QList<T>"] = { "QList<QString>", "QList<QFileInfo>", "QList<QVariant>" },
 	},
 	qtgui = {
-		["QList<T>"] = { "QList<QGraphicsItem*>" },
-		["QVector<T>"] = { "QVector<QPointF>" },
+		["QList<T>"] = { "QList<QGraphicsItem*>", "QList<int>", "QList<qreal>",
+			"QList<QModelIndex>", "QList<QSize>", "QList<QPolygonF>", "QList<QKeySequence>",
+			"QList<QUrl>" },
+		["QVector<T>"] = { "QVector<QPointF>", "QVector<QPoint>", "QVector<QRgb>", "QVector<QLine>",
+			"QList<QRectF>", "QVector<QTextLength>", "QVector<QGradientStop>" },
+	},
+	qtnetwork = {
+		["QList<T>"] = { "QList<QSslError>", "QList<QSslCertificate>", "QList<QNetworkCookie>",
+			"QList<QSslCipher>", "QList<QNetworkAddressEntry>", "QList<QNetworkProxy>", 
+			"QList<QHostAddress>", }
 	},
 }
