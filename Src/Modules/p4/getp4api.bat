84ec13f7a16af2900c3ed2e1b124013611cede2c
Joshua Jensen <jjensen@workspacewhiz.com>
2010-11-29 20:57:39 -0700

    * Fix the build numbers in getp4api.bat.

--- Src/Modules/p4/getp4api.bat
+++ Src/Modules/p4/getp4api.bat
@@ -4,19 +4,19 @@ if exist p4api_vs2005_dyn.zip del p4api_vs2005_dyn.zip
 if exist p4api-2008.2.179173-vs2005_dyn rmdir /s /q p4api-2008.2.179173-vs2005_dyn
 if exist p4api_vs2008_dyn.zip del p4api_vs2008_dyn.zip
 if exist p4api-2009.1.209442-vs2008_dyn rmdir /s /q p4api-2009.1.209442-vs2008_dyn
-if exist p4api-2010.1.260003-vs2008_dyn rmdir /s /q p4api-2010.1.260003-vs2008_dyn
+if exist p4api-2010.1.265509-vs2008_dyn rmdir /s /q p4api-2010.1.265509-vs2008_dyn
 wget ftp://ftp.perforce.com/perforce/r10.1/bin.ntx86/p4api_vs2008_dyn.zip
 unzip p4api_vs2008_dyn.zip
-ren p4api-2010.1.260003-vs2008_dyn p4api\release
+ren p4api-2010.1.265509-vs2008_dyn p4api\release
 del p4api_vs2008_dyn.zip
 
 if exist p4api_vs2005_dyn_vsdebug.zip del p4api_vs2005_dyn_vsdebug.zip
 if exist p4api-2008.2.179173-vs2005_dyn_vsdebug rmdir /s /q p4api-2008.2.179173-vs2005_dyn_vsdebug
 if exist p4api_vs2008_dyn_vsdebug.zip del p4api_vs2008_dyn_vsdebug.zip
 if exist p4api-2009.1.209442-vs2008_dyn_vsdebug rmdir /s /q p4api-2009.1.209442-vs2008_dyn_vsdebug
-if exist p4api-2010.1.260003-vs2008_dyn_vsdebug rmdir /s /q p4api-2010.1.260003-vs2008_dyn_vsdebug
+if exist p4api-2010.1.265509-vs2008_dyn_vsdebug rmdir /s /q p4api-2010.1.265509-vs2008_dyn_vsdebug
 wget ftp://ftp.perforce.com/perforce/r10.1/bin.ntx86/p4api_vs2008_dyn_vsdebug.zip
 unzip p4api_vs2008_dyn_vsdebug.zip
-ren p4api-2010.1.260003-vs2008_dyn_vsdebug p4api\debug
+ren p4api-2010.1.265509-vs2008_dyn_vsdebug p4api\debug
 del p4api_vs2008_dyn_vsdebug.zip
 
