5c0a7c468334950689471402422af0f94b48e552
Joshua Jensen <jjensen@workspacewhiz.com>
2011-01-14 15:37:35 -0700

    * python module: Use new Lua DLL names.

--- Src/Modules/python/src/luainpython.c
+++ Src/Modules/python/src/luainpython.c
@@ -523,9 +523,9 @@ initlua(void)
 						strcpy(slashptr, buffer);
 
 #ifdef _DEBUG
-					strcat(filename, "luaplus_1100.debug.dll");
+					strcat(filename, "luaplus51-1200_debug.dll");
 #else
-					strcat(filename, "luaplus_1100.dll");
+					strcat(filename, "luaplus51-1200.dll");
 #endif
 					LoadLibrary(filename);
 				}
