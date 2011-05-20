53d383c94f8fbdaf81f30c2bd987b8d28f2a8e38
Joshua Jensen <jjensen@workspacewhiz.com>
2011-05-19 22:17:15 -0600

    * p4 module: Fix getp4api_vs2010.bat against the latest P4API.

--- Src/Modules/p4/getp4api_vs2010.bat
+++ Src/Modules/p4/getp4api_vs2010.bat
@@ -1,18 +1,17 @@
 if exist p4api rmdir /s /q p4api
 mkdir p4api
 if exist p4api_vs2010_dyn.zip del p4api_vs2010_dyn.zip
-if exist p4api-2010.1.260003-vs2010_dyn rmdir /s /q p4api-2010.1.260003-vs2010_dyn
+if exist p4api-2010.1.265509-vs2010_dyn rmdir /s /q p4api-2010.1.265509-vs2010_dyn
 wget ftp://ftp.perforce.com/perforce/r10.1/bin.ntx86/p4api_vs2010_dyn.zip
 unzip p4api_vs2010_dyn.zip
-if exist p4api-2010.1.259176-vs2010_dyn rmdir /s /q p4api-2010.1.259176-vs2010_dyn
-ren p4api-2010.1.260003-vs2010_dyn p4api\release
+ren p4api-2010.1.265509-vs2010_dyn p4api\release
 del p4api_vs2010_dyn.zip
 
 if exist p4api_vs2010_dyn_vsdebug.zip del p4api_vs2010_dyn_vsdebug.zip
-if exist p4api-2010.1.260003-vs2010_dyn_vsdebug rmdir /s /q p4api-2010.1.260003-vs2010_dyn_vsdebug
+if exist p4api-2010.1.265509-vs2010_dyn_vsdebug rmdir /s /q p4api-2010.1.260003-vs2010_dyn_vsdebug
 wget ftp://ftp.perforce.com/perforce/r10.1/bin.ntx86/p4api_vs2010_dyn_vsdebug.zip
 unzip p4api_vs2010_dyn_vsdebug.zip
-if exist p4api-2010.1.259176-vs2010_dyn_vsdebug rmdir /s /q p4api-2010.1.259176-vs2010_dyn_vsdebug
-ren p4api-2010.1.260003-vs2010_dyn_vsdebug p4api\debug
+if exist p4api-2010.1.265509-vs2010_dyn_vsdebug rmdir /s /q p4api-2010.1.259176-vs2010_dyn_vsdebug
+ren p4api-2010.1.265509-vs2010_dyn_vsdebug p4api\debug
 del p4api_vs2010_dyn_vsdebug.zip
 
