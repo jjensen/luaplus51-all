cc7c1f30dfa83a26d3ed404df8def4da1728873c
Joshua Jensen <jjensen@workspacewhiz.com>
2011-08-27 12:39:18 -0600

    * Fix website URL at top of file.

--- Src/LuaPlus/lwstrlib.c
+++ Src/LuaPlus/lwstrlib.c
@@ -2,7 +2,7 @@
 // This source file is part of the LuaPlus source distribution and is Copyright
 // 2001-2011 by Joshua C. Jensen (jjensen@workspacewhiz.com).
 //
-// The latest version may be obtained from http://wwhiz.com/LuaPlus/.
+// The latest version may be obtained from http://luaplus.org/.
 //
 // The code presented in this file may be used in any environment it is
 // acceptable to use Lua.
