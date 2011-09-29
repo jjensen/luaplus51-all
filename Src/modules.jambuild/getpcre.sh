d41ef44967312fc0279d586750a201057674ae1d
Joshua Jensen <jjensen@workspacewhiz.com>
2011-09-29 17:11:29 -0600

    * lrexlib: Update the PCRE build number.

--- Src/modules.jambuild/getpcre.sh
+++ Src/modules.jambuild/getpcre.sh
@@ -1,24 +1,12 @@
 cd ../Modules/lrexlib
-if [ -f pcre-8.02.tar.bz2 ]
+if [ -d pcre-8.13 ]
 then
-	rm pcre-8.02.tar.bz2
+	rm -rf pcre-8.13
 fi
-if [ -f pcre-8.00.tar.bz2 ]
-then
-	rm pcre-8.00.tar.bz2
-fi
-if [ -f pcre-7.8.tar.bz2 ]
-then
-	rm pcre-7.8.tar.bz2
-fi
-if [ -d pcre-7.8 ]
-then
-	rm -rf pcre-7.8
-fi
-curl ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.02.tar.bz2 -o pcre-8.02.tar.bz2
-tar -xjf pcre-8.02.tar.bz2
+curl ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.13.tar.bz2 -o pcre-8.13.tar.bz2
+tar -xjf pcre-8.13.tar.bz2
 
-cd pcre-8.02
+cd pcre-8.13
 ./configure
 
 if ! [ -f pcre_chartables.c ]
@@ -28,4 +16,4 @@ fi
 
 cd ..
 
-rm pcre-8.02.tar.bz2
+rm pcre-8.13.tar.bz2
