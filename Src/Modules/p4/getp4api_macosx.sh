5c2c9011b1915e31fc4978045573fdf5c54b3b55
Joshua Jensen <jjensen@workspacewhiz.com>
2011-09-29 17:11:06 -0600

    * p4: Fix the p4api version number in getp4api_macosx.sh.

--- Src/Modules/p4/getp4api_macosx.sh
+++ Src/Modules/p4/getp4api_macosx.sh
@@ -1,9 +1,12 @@
 rm -rf p4api
 mkdir p4api
-if [ -f p4api.tgz ] then
+if [ -f p4api.tgz ]
+then
 	rm p4api.tgz
 fi
 curl ftp://ftp.perforce.com/perforce/r10.2/bin.macosx105u/p4api.tgz -o p4api.tgz
 tar xvfz p4api.tgz
-mv -f p4api-2010.2.277537 p4api
+mv -f p4api-2010.2.295040/* p4api
+rm -rf p4api-2010.2.295040
+rm p4api.tgz
 
