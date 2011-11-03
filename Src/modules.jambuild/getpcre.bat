1c0a101ba31b03518558ca87feb12c2f6122c6bb
Joshua Jensen <jjensen@workspacewhiz.com>
2011-11-03 00:15:11 -0600

    * pcre module: Fix getpcre.bat.

--- Src/modules.jambuild/getpcre.bat
+++ Src/modules.jambuild/getpcre.bat
@@ -6,11 +6,13 @@ if exist pcre-8.00.zip del pcre-8.00.zip
 if exist pcre-8.00 rmdir /s /q pcre-8.00
 if exist pcre-8.02.zip del pcre-8.02.zip
 if exist pcre-8.02 rmdir /s /q pcre-8.02
-wget ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.02.zip
-unzip pcre-8.02.zip
-del pcre-8.02.zip
+if exist pcre-8.13.zip del pcre-8.13.zip
+if exist pcre-8.13 rmdir /s /q pcre-8.13
+wget ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.13.zip
+unzip pcre-8.13.zip
+del pcre-8.13.zip
 
-cd pcre-8.02
+cd pcre-8.13
 if not exist pcre.h copy pcre.h.generic pcre.h
 if not exist config.h copy config.h.generic config.h
 if not exist pcre_chartables.c copy pcre_chartables.c.dist pcre_chartables.c
