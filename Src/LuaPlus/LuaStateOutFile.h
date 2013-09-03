4f27949b22b6e55223eaa61e05f49e919d0775df
Joshua Jensen <jjensen@workspacewhiz.com>
2013-09-03 12:42:35 -0600

    Make LuaStateOutFile completely inline and not exported from a DLL

--- Src/LuaPlus/LuaStateOutFile.h
+++ Src/LuaPlus/LuaStateOutFile.h
@@ -33,14 +33,14 @@ namespace LuaPlus
 class LuaStateOutFile
 {
 public:
-	LUAPLUS_CLASS_API LuaStateOutFile();
-	LUAPLUS_CLASS_API LuaStateOutFile(const char* fileName);
-	LUAPLUS_CLASS_API virtual ~LuaStateOutFile();
-	LUAPLUS_CLASS_API virtual bool Open( const char* fileName );
-	LUAPLUS_CLASS_API virtual void Close();
-	LUAPLUS_CLASS_API virtual void Print( const char* str, ... );
-	LUAPLUS_CLASS_API bool Assign( FILE* file );
-	LUAPLUS_CLASS_API void Indent( unsigned int indentLevel );
+	LuaStateOutFile();
+	LuaStateOutFile(const char* fileName);
+	virtual ~LuaStateOutFile();
+	virtual bool Open( const char* fileName );
+	virtual void Close();
+	virtual void Print( const char* str, ... );
+	bool Assign( FILE* file );
+	void Indent( unsigned int indentLevel );
 
 protected:
 	FILE* m_file;
