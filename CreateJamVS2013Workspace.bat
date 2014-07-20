008df233815e7839bb87f0f25227fa862a629ad9
Joshua Jensen <jjensen@workspacewhiz.com>
2014-07-20 08:22:04 -0600

    Add ability to have directory extension in CreateJamVS2013Workspace.bat

--- CreateJamVS2013Workspace.bat
+++ CreateJamVS2013Workspace.bat
@@ -1,1 +1,1 @@
-@jam --workspace -gen=vs2013 --compiler=vs2013 -config=CreateJamWindowsWorkspace.config Jamfile.jam build2013
+@jam --workspace -gen=vs2013 --compiler=vs2013 -config=CreateJamWindowsWorkspace.config Jamfile.jam build2013%1
