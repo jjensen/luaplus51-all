7b2b0c584aa2339ceed29a2827675be31e7a996d
Joshua Jensen <jjensen@workspacewhiz.com>
2014-07-24 11:16:56 -0600

    lua52-luaplus: Support lua.link

--- Src/LuaPlus/lua52-luaplus/src/lua.c
+++ Src/LuaPlus/lua52-luaplus/src/lua.c
@@ -5,6 +5,10 @@
 */
 
 
+#if defined(_WIN32)
+#include <windows.h>
+#endif
+
 // wxLua added to enable visual styles, the modern Win7+ controls in v6 of comctrl32.dll.
 #ifdef _MSC_VER
     #pragma comment(lib,"comctl32.lib")
@@ -544,6 +548,46 @@ static int pmain (lua_State *L) {
 int main (int argc, char **argv) {
   int status, result;
   lua_State *L;
+#if defined(_WIN32)
+  {
+    FILE* file;
+    char filename[_MAX_PATH];
+    char* slashptr;
+    GetModuleFileName(NULL, filename, _MAX_PATH);
+    slashptr = strrchr(filename, '\\');
+    if (slashptr) {
+      slashptr++;
+      strcpy(slashptr, "lua.link");
+      file = fopen(filename, "r");
+      if (file) {
+        char buffer[ MAX_PATH ];
+        if (fgets(buffer, sizeof(buffer), file) != NULL) {
+          char *ptr = buffer + strlen(buffer) - 1;
+          while (ptr != buffer  &&  (*ptr == '\n'  ||  *ptr == '\r'))
+            *ptr-- = 0;
+          ++ptr;
+          if (*ptr != '/'  ||  *ptr != '\\') {
+            *ptr++ = '/';
+            *ptr = 0;
+          }
+
+          if (buffer[0] == '/'  ||  (buffer[1]  &&  buffer[1] == ':'))
+            strcpy(filename, buffer);
+          else
+            strcpy(slashptr, buffer);
+
+#ifdef _DEBUG
+          strcat(filename, "lua52_debug.dll");
+#else
+          strcat(filename, "lua52.dll");
+#endif
+          LoadLibrary(filename);
+        }
+        fclose(file);
+      }
+    }
+  }
+#endif
   L = luaL_newstate();  /* create state */
   if (L == NULL) {
     l_message(argv[0], "cannot create state: not enough memory");
