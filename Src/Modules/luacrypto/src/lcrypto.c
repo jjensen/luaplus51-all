03c2921b1c15e92f6a1f0c4bdb6c5828eafe2df1
Joshua Jensen <jjensen@workspacewhiz.com>
2014-12-26 11:47:39 -0700

    luacrypto: Compile against Lua 5.3

--- Src/Modules/luacrypto/src/lcrypto.c
+++ Src/Modules/luacrypto/src/lcrypto.c
@@ -968,7 +968,7 @@ static int verify_fverify(lua_State *L)
 
 static int rand_do_bytes(lua_State *L, int (*bytes)(unsigned char *, int))
 {
-    size_t count = (size_t)luaL_checkint(L, 1);
+    size_t count = (size_t)luaL_checkinteger(L, 1);
     unsigned char tmp[256], *buf = tmp;
     if (count > sizeof tmp)
         buf = (unsigned char *)malloc(count);
