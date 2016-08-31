677293bb64440387416101d0cbb74e122e1b96d8
Joshua Jensen <jjensen@workspacewhiz.com>
2016-08-31 11:30:31 -0600

    Bootstrapping: Bootstrap the Mac OS X JamPlus automatically

--- CreateJamXcodeWorkspace.sh
+++ CreateJamXcodeWorkspace.sh
@@ -1,1 +1,2 @@
-jam --workspace -gen=xcode -config=CreateJamXcodeWorkspace.config Jamfile.jam .build
+./BootstrapJamPlus-macosx64
+Tools/JamPlus/bin/macosx64/jam --workspace -gen=xcode -config=CreateJamXcodeWorkspace.config Jamfile.jam .build
